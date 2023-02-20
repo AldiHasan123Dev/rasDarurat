e7b2dbdf8f421fd4ff4f74ad5bbe48069baa6ff8
Spyder-code <luaysyauqillah@gmail.com>
2023-02-20 11:57:22 +0700

    update fix crud

--- app/Console/Commands/CRUD.php
+++ app/Console/Commands/CRUD.php
@@ -62,7 +62,7 @@ class CRUD extends Command
                 $this->createMigration($data,$model_name);
                 Artisan::call('controller:model', ['name' => $model_name ]);
                 $resource = "Route::resource('".strtolower($model_name)."',App\\Http\\Controllers\\".$model_name."Controller::class);";
-                $datatable = "Route::get('".strtolower($model_name)."',[App\\Http\\Controllers\\".$model_name."Controller::class,'datatable'])->name('".strtolower($model_name).".data');";
+                $datatable = "Route::post('".strtolower($model_name)."',[App\\Http\\Controllers\\".$model_name."Controller::class,'datatable'])->name('".strtolower($model_name).".data');";
                 $fp = fopen(base_path('routes/web.php'), 'a');
                 $fd = fopen(base_path('routes/datatable.php'), 'a');
                 fwrite($fp, $resource);
@@ -70,12 +70,12 @@ class CRUD extends Command
                 // $menu = "<li class=\"sidebar-item\">\r\n    <a class=\"sidebar-link waves-effect waves-dark sidebar-link\" href=\"{{ route('".strtolower($model_name).".index') }}\"\r\n        aria-expanded=\"false\">\r\n        <i class=\"fa fa-list\" aria-hidden=\"true\"></i>\r\n        <span class=\"hide-menu\">".$model_name." Management</span>\r\n    </a>\r\n</li>";
                 // $fp = fopen(base_path('resources/views/component/sidebar.blade.php'), 'a');
                 // fwrite($fp, $menu);
-                if (!file_exists(base_path('resources/views/desktop'))) {
-                    mkdir(base_path('resources/views/desktop'), 0777);
+                if (!file_exists(base_path('resources/views/admin'))) {
+                    mkdir(base_path('resources/views/admin'), 0777);
                 }
 
-                if (!file_exists(base_path('resources/views/desktop/'.strtolower($model_name)))) {
-                    mkdir(base_path('resources/views/desktop/'.strtolower($model_name)), 0777);
+                if (!file_exists(base_path('resources/views/admin/'.strtolower($model_name)))) {
+                    mkdir(base_path('resources/views/admin/'.strtolower($model_name)), 0777);
                     // $this->viewCreate($model_name);
                     // $this->viewEdit($model_name);
                     $this->viewIndex($data, $model_name);
@@ -123,7 +123,7 @@ class CRUD extends Command
             }
 
         };
-        $html = "<?php\r\n\r\nuse Illuminate\\Database\\Migrations\\Migration;\r\nuse Illuminate\\Database\\Schema\\Blueprint;\r\nuse Illuminate\\Support\\Facades\\Schema;\r\n\r\nreturn new class extends Migration\r\n{\r\n    public function up()\r\n    {\r\n        Schema::create('".strtolower($model_name)."s', function (Blueprint ".'$table'.") {\r\n            ".'$table->id()'.";".$str."\r\n            ".'$table->timestamps()'.";\r\n            ".'$ table->softDeletes();'.";\r\n            ".'$table->timestamps()'.";\r\n        });\r\n    }\r\n\r\n    public function down()\r\n    {\r\n        Schema::dropIfExists('".strtolower($model_name)."s');\r\n    }\r\n}";
+        $html = "<?php\r\n\r\nuse Illuminate\\Database\\Migrations\\Migration;\r\nuse Illuminate\\Database\\Schema\\Blueprint;\r\nuse Illuminate\\Support\\Facades\\Schema;\r\n\r\nreturn new class extends Migration\r\n{\r\n    public function up(): void\r\n    {\r\n        Schema::create('".strtolower($model_name)."s', function (Blueprint ".'$table'.") {\r\n            ".'$table->id()'.";".$str."\r\n            ".'$table->softDeletes()'.";\r\n            ".'$table->timestamps()'.";\r\n        });\r\n    }\r\n\r\n    public function down(): void\r\n    {\r\n        Schema::dropIfExists('".strtolower($model_name)."s');\r\n    }\r\n};";
         $file_name = date('Y_m_d_His').'_create_'.strtolower($model_name).'s_table';
         $fp = fopen(base_path('database')."/"."migrations/".$file_name.".php","wb");
         fwrite($fp,$html);
@@ -157,9 +157,9 @@ class CRUD extends Command
             $column = $column."\r\n            { data: '".strtolower($item->attr)."', name: '".strtolower($item->attr)."' },";
         };
 
-        $content = "@extends('layouts.admin')\r\n@section('content')\r\n    <div class=\"container mt-3\">\r\n        <div class=\"card\">\r\n            <div class=\"card-header p-2 d-flex justify-content-between\" style=\"gap:10px\">\r\n                <button class=\"py-2 px-3 btn btn-success\" data-bs-toggle=\"offcanvas\" data-bs-target=\"#offcanvas".$model_name."\" aria-controls=\"offcanvas".$model_name."\">Tambah ".$model_name."<\/button>\r\n            <\/div>\r\n            <div class=\"card-body\">\r\n                <div class=\"table-responsive\">\r\n                    <table class=\"table table-sm\" style=\"font-size:.7rem\">\r\n                        <thead>\r\n                            <tr>\r\n                                <th>ID.<\/th>\r\n".$header."                            <\/tr>\r\n                        <\/thead>\r\n                        <tbody>\r\n                        <\/tbody>\r\n                    <\/table>\r\n                <\/div>\r\n            <\/div>\r\n        <\/div>\r\n    <\/div>\r\n\r\n\r\n    <div class=\"offcanvas offcanvas-start\" tabindex=\"-2\" id=\"offcanvas".$model_name."\" aria-labelledby=\"offcanvas".$model_name."Label\">\r\n        <div class=\"offcanvas-header\">\r\n            <h5 class=\"offcanvas-title\" id=\"offcanvas".$model_name."Label\">Form ".$model_name."<\/h5>\r\n            <button type=\"button\" class=\"btn-close text-reset\" data-bs-dismiss=\"offcanvas\" aria-label=\"Close\"><\/button>\r\n        <\/div>\r\n        <div class=\"offcanvas-body\">\r\n            <form action=\"{{ route('".strtolower($model_name).".store') }}\" method=\"post\">\r\n                @csrf\r\n                @include('admin.".strtolower($model_name).".form')\r\n            <\/form>\r\n        <\/div>\r\n    <\/div>\r\n@endsection\r\n\r\n@section('script')\r\n    <script>\r\n        let table = $('.table').DataTable({\r\n            processing: true,\r\n            serverSide: true,\r\n            ajax:{\r\n                url: '{{ route('".strtolower($model_name).".data') }}',\r\n                method:'POST',\r\n                headers: {'X-CSRF-TOKEN': $('meta[name=\"csrf-token\"]').attr('content')},\r\n            },\r\n            columns: [\r\n                { data: 'id', name: 'id' },".$column."\r\n                { data: 'action', name: 'action', orderable: false, searchable: false },\r\n            ]\r\n        });\r\n    <\/script>\r\n@endsection";
+        $content = "@extends('layouts.admin')\r\n@section('content')\r\n    <div class=\"container mt-3\">\r\n        <div class=\"card\">\r\n            <div class=\"card-header p-2 d-flex justify-content-between\" style=\"gap:10px\">\r\n                <button class=\"py-2 px-3 btn btn-success\" data-bs-toggle=\"offcanvas\" data-bs-target=\"#offcanvas".$model_name."\" aria-controls=\"offcanvas".$model_name."\">Tambah ".$model_name."</button>\r\n            </div>\r\n            <div class=\"card-body\">\r\n                <div class=\"table-responsive\">\r\n                    <table class=\"table table-sm\" style=\"font-size:.7rem\">\r\n                        <thead>\r\n                            <tr>\r\n                                <th>ID.</th>\r\n".$header."                                <th>Action</th>\r\n                            </tr>\r\n                        </thead>\r\n                        <tbody>\r\n                        </tbody>\r\n                    </table>\r\n                </div>\r\n            </div>\r\n        </div>\r\n    </div>\r\n\r\n\r\n    <div class=\"offcanvas offcanvas-start\" tabindex=\"-2\" id=\"offcanvas".$model_name."\" aria-labelledby=\"offcanvas".$model_name."Label\">\r\n        <div class=\"offcanvas-header\">\r\n            <h5 class=\"offcanvas-title\" id=\"offcanvas".$model_name."Label\">Form ".$model_name."</h5>\r\n            <button type=\"button\" class=\"btn-close text-reset\" data-bs-dismiss=\"offcanvas\" aria-label=\"Close\"></button>\r\n        </div>\r\n        <div class=\"offcanvas-body\">\r\n            <form action=\"{{ route('".strtolower($model_name).".store') }}\" method=\"post\">\r\n                @csrf\r\n                @include('admin.".strtolower($model_name).".form')\r\n            </form>\r\n        </div>\r\n    </div>\r\n@endsection\r\n\r\n@section('script')\r\n    <script>\r\n        let table = $('.table').DataTable({\r\n            processing: true,\r\n            serverSide: true,\r\n            ajax:{\r\n                url: '{{ route('".strtolower($model_name).".data') }}',\r\n                method:'POST',\r\n                headers: {'X-CSRF-TOKEN': $('meta[name=\"csrf-token\"]').attr('content')},\r\n            },\r\n            columns: [\r\n                { data: 'id', name: 'id' },".$column."\r\n                { data: 'action', name: 'action', orderable: false, searchable: false },\r\n            ]\r\n        });\r\n    </script>\r\n@endsection";
 
-        $index = fopen(base_path('resources/views/desktop/'.strtolower($model_name))."/index.blade.php","wb");
+        $index = fopen(base_path('resources/views/admin/'.strtolower($model_name))."/index.blade.php","wb");
         fwrite($index, $content);
         fclose($index);
     }
@@ -177,9 +177,9 @@ class CRUD extends Command
             }
         };
 
-        $content = "<x-error-validation/>\r\n".$input."";
+        $content = "<div class=\"row\">".$input."\r\n<div class=\"col-12 mb-2 px-1\">\r\n    <button type=\"submit\" class=\"btn btn-success btn-sm\">{{ empty($".strtolower($model_name).")?'Tambah':'Update' }} Data</button>\r\n</div>\r\n</div>";
 
-        $form = fopen(base_path('resources/views/desktop/'.strtolower($model_name))."/form.blade.php","wb");
+        $form = fopen(base_path('resources/views/admin/'.strtolower($model_name))."/form.blade.php","wb");
         fwrite($form, $content);
         fclose($form);
     }
