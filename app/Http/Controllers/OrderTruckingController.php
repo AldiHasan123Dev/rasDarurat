59f0dff66832040abb0601ca74bd36ee2838b340
Spyder-code <luaysyauqillah@gmail.com>
2023-05-17 16:01:18 +0700

    update ukuran

--- app/Http/Controllers/OrderTruckingController.php
+++ app/Http/Controllers/OrderTruckingController.php
@@ -151,8 +151,20 @@ class OrderTruckingController extends Controller
                 if(!$tarif){
                     return back()->with('danger','Master Tarif Customer belum dibuat! Harap input master tarif terlebih dahulu dan pastikan tarif berstatus Aktif!');
                 }
-                if($sangu->ukuran_20!=$ordertrucking->ukuran_20 || $sangu->ukuran_40!=$ordertrucking->ukuran_40 || $sangu->ukuran_combo!=$ordertrucking->ukuran_combo){
-                    return back()->with('danger','Tidak bisa update data karena borongan sopir tidak sama!');
+                if($ordertrucking->tipe==20){
+                    if($sangu->ukuran_20!=$ordertrucking->ukuran_20){
+                        return back()->with('danger','Tidak bisa update data karena borongan sopir tidak sama!');
+                    }
+                }
+                if($ordertrucking->tipe==40){
+                    if($sangu->ukuran_40!=$ordertrucking->ukuran_40){
+                        return back()->with('danger','Tidak bisa update data karena borongan sopir tidak sama!');
+                    }
+                }
+                if($ordertrucking->tipe=='COMBO'){
+                    if($sangu->ukuran_combo!=$ordertrucking->ukuran_combo){
+                        return back()->with('danger','Tidak bisa update data karena borongan sopir tidak sama!');
+                    }
                 }
                 $data['tujuan'] = $sangu->tujuanInfo->nama;
                 $data['tarif_id'] = $tarif->id;
