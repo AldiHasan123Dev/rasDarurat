4f6aa226fe08a973e607161ab098ff509423aa50
Spyder-code <luaysyauqillah@gmail.com>
2023-07-14 07:53:47 +0700

    show import

--- resources/views/admin/jurnal/index.blade.php
+++ resources/views/admin/jurnal/index.blade.php
@@ -22,10 +22,10 @@
                 <a href="{{ route('jurnal.kolektif.create') }}" class="py-2 px-3 btn btn-primary">Tambah Jurnal Group JOB</a>
                 <a href="{{ route('jurnal.balik.create') }}" class="py-2 px-3 btn btn-warning">Tambah Jurnal Balik</a>
                 <a href="{{ route('jurnal.merge') }}" class="py-2 px-3 btn btn-secondary">Merge No. Jurnal</a>
-                {{-- <form action="{{ route('jurnal.import') }}" method="post" enctype="multipart/form-data">
+                <form action="{{ route('jurnal.import') }}" method="post" enctype="multipart/form-data">
                     @csrf
                     <input type="file" name="file" id="file" onchange="submit()">
-                </form> --}}
+                </form>
             </div>
             <div class="card-body">
                 <livewire:list-jurnal :month="request('month')" :tipe="request('tipe')"/>
