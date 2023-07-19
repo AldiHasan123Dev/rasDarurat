f2afe6496b8719cd9714bbebab2946c7f4286d44
Spyder-code <luaysyauqillah@gmail.com>
2023-07-20 06:07:39 +0700

    update neraca

--- app/Http/Livewire/Neraca.php
+++ app/Http/Livewire/Neraca.php
@@ -17,11 +17,12 @@ class Neraca extends Component
         $this->months = ['Jan','Feb','Mar','Apr','Mei','Jun','Jul','Agu','Sep','Okt','Nov','Des'];
         $this->month = date('m');
         $this->year = date('Y');
-        $m = (int)$this->month -1;
+        $m = sprintf('%02d',(int)$this->month -1);
         $this->start = $this->year.'-'.$m.'-01';
         if($this->month=='01'){
             $this->start = ((int)$this->year - 1).'-12-01';
         }
+        $this->start = '2022-12-01';
         $this->end = $this->getLastDay();
         $this->aktiva_lancar = COA::whereIn('kode',['1','1.1','1.1.1','1.1.1.1','1.1.2','1.1.2.1','1.1.3','1.1.3.1','1.1.3.2','1.1.3.3','1.1.4','1.1.4.1','1.1.4.2','1.1.5','1.1.5.1','1.1.5.2','1.1.5.3','1.1.5.4','1.1.6','1.1.6.1','1.1.6.2','1.3','1.4','1.5','1.6','1.7','1.8','1.3.1','1.3.2','1.3.3','1.3.4','1.3.5','1.3.6','1.4.1','1.4.2','1.4.3','1.4.4','1.6.1','1.6.2','1.6.3','1.7.1','1.7.2','1.8.1','1.8.2','1.8.3','1.4.1.1','1.6.2.1','1.6.2.2'])->orderBy('kode')->get();
         $this->aktiva_tak_lancar = COA::whereIn('kode',['1.2','1.2.1','1.2.1.1','1.2.1.1.1','1.2.1.2','1.2.1.2.1'])->orderBy('kode')->get();
@@ -40,7 +41,7 @@ class Neraca extends Component
                 ->whereBetween('jurnal.created_at',[$this->start,$this->end])
                 ->get();
         $this->lr = ($kel5->sum('credit') - $kel5->sum('debit')) - (($kel6->sum('debit') - $kel6->sum('credit')) + ($kel7->sum('debit') - $kel7->sum('credit')));
-
+        // dd($this->start,$this->end);
     }
 
     public function render()
@@ -52,11 +53,11 @@ class Neraca extends Component
     {
         $m = sprintf('%02d',$month);
         $this->month = $m;
-        $s = (int)$this->month -1;
-        $this->start = $this->year.'-'.$s.'-01';
-        if($this->month=='01'){
-            $this->start = ((int)$this->year - 1).'-12-01';
-        }
+        // $s = (int)$this->month -1;
+        // $this->start = $this->year.'-'.$s.'-01';
+        // if($this->month=='01'){
+        //     $this->start = ((int)$this->year - 1).'-12-01';
+        // }
         $this->end = $this->getLastDay();
         $this->modal = COA::whereIn('kode',['3','3.1','3.2','3.3'])->orderBy('kode')->get();
         $kel5 = Jurnal::join('coa','coa.id','=','jurnal.coa_id')
@@ -76,11 +77,11 @@ class Neraca extends Component
 
     public function changeYear()
     {
-        $s = (int)$this->month -1;
-        $this->start = $this->year.'-'.$s.'-01';
-        if($this->month=='01'){
-            $this->start = ((int)$this->year - 1).'-12-01';
-        }
+        // $s = (int)$this->month -1;
+        // $this->start = $this->year.'-'.$s.'-01';
+        // if($this->month=='01'){
+        //     $this->start = ((int)$this->year - 1).'-12-01';
+        // }
         $this->end = $this->getLastDay();
         $this->modal = COA::whereIn('kode',['3','3.1','3.2','3.3'])->orderBy('kode')->get();
         $kel5 = Jurnal::join('coa','coa.id','=','jurnal.coa_id')
