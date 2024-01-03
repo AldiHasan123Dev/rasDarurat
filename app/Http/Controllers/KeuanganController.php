24d87b4ca31d2901c64f2793414fe5d947c81124
Spyder-code <luaysyauqillah@gmail.com>
2024-01-03 13:22:17 +0700

    update periode invoice

--- app/Http/Controllers/KeuanganController.php
+++ app/Http/Controllers/KeuanganController.php
@@ -118,7 +118,7 @@ class KeuanganController extends Controller
         if (!$nsfp) {
             return back()->with('danger','Tidak ada NSFP yang tersedia! Harap input NSFP terlebih dahulu');
         }
-        $no = Transaksi::whereYear('created_at',date('Y'))->max('order') + 1;
+        $no = Transaksi::max('order') + 1;
         $roman_numerals = array("", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII"); // daftar angka Romawi
         $month_number = date("n"); // mengambil nomor bulan dari tanggal
         $month_roman = $roman_numerals[$month_number]; // mengambil angka Romawi yang sesuai
