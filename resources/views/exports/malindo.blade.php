95b7750b12fcfbe55355badf8eda0d29c1b8e388
Spyder-code <luaysyauqillah@gmail.com>
2024-07-04 08:00:50 +0700

    update export malindo

--- resources/views/exports/malindo.blade.php
+++ resources/views/exports/malindo.blade.php
@@ -2,30 +2,26 @@
     <thead>
         <tr>
             <th>No.</th>
+            <th>No. JOB</th>
             <th>Carton</th>
             <th>Tgl Stuffing</th>
             <th>No. Cont</th>
             <th>Nopol Truck</th>
             <th>Kapal</th>
             <th>TD</th>
-            <th>ETA</th>
-            <th>TA</th>
-            <th>Tgl Dooring</th>
         </tr>
     </thead>
     <tbody>
         @foreach ($data as $item)
             <tr>
                 <td>{{ $loop->iteration }}</td>
+                <td>{{ $item['no'] }}</td>
                 <td>{{ $item['koli'] }}</td>
                 <td>{{ $item['stuffing'] }}</td>
                 <td>{{ $item['container'] }}</td>
                 <td>{{ $item['nopol'] }}</td>
                 <td>{{ $item['kapal'] }}</td>
                 <td>{{ $item['td'] }}</td>
-                <td></td>
-                <td></td>
-                <td></td>
             </tr>
         @endforeach
     </tbody>
