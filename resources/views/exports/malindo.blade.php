ab896b483a18beb64b67ea47832b5fa30f998915
Spyder-code <luaysyauqillah@gmail.com>
2024-07-06 10:03:52 +0700

    change order column

--- resources/views/exports/malindo.blade.php
+++ resources/views/exports/malindo.blade.php
@@ -3,13 +3,13 @@
         <tr>
             <th>No.</th>
             <th>JOB</th>
-            <th>RC Cust</th>
             <th>Carton</th>
             <th>Tgl Stuffing</th>
             <th>No. Cont</th>
             <th>Nopol Truck</th>
             <th>Kapal</th>
             <th>TD</th>
+            <th>RC Cust</th>
         </tr>
     </thead>
     <tbody>
@@ -17,13 +17,13 @@
             <tr>
                 <td>{{ $loop->iteration }}</td>
                 <td>{{ $item['job'] }}</td>
-                <td>{{ $item['komisi'] }}</td>
                 <td>{{ $item['koli'] }}</td>
                 <td>{{ $item['stuffing'] }}</td>
                 <td>{{ $item['container'] }}</td>
                 <td>{{ $item['nopol'] }}</td>
                 <td>{{ $item['kapal'] }}</td>
                 <td>{{ $item['td'] }}</td>
+                <td>{{ $item['komisi'] }}</td>
             </tr>
         @endforeach
     </tbody>
