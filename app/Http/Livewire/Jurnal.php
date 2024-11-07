a3cd0d1998b8531067dab1bd1c91831906c4693d
Spyder-code <luaysyauqillah@gmail.com>
2024-11-07 09:06:43 +0700

    update livewire

--- app/Http/Livewire/Jurnal.php
+++ app/Http/Livewire/Jurnal.php
@@ -21,13 +21,13 @@ class Jurnal extends Component
 
     public function mount()
     {
-        $no_1 = ModelsJurnal::where('tipe','JNL')->whereMonth('created_at',date('m'))->whereYear('created_at',date('Y'))->max('no') + 1;
-        $no_2 = ModelsJurnal::where('tipe','BBK')->whereYear('created_at',date('Y'))->max('no') + 1;
-        $no_3 = ModelsJurnal::where('tipe','BBM')->whereYear('created_at',date('Y'))->max('no') + 1;
-        $no_4 = ModelsJurnal::where('tipe','BKK')->whereYear('created_at',date('Y'))->max('no') + 1;
-        $no_5 = ModelsJurnal::where('tipe','BKM')->whereYear('created_at',date('Y'))->max('no') + 1;
-        $no_6 = ModelsJurnal::where('tipe','BBKT')->whereYear('created_at',date('Y'))->max('no') + 1;
-        $no_7 = ModelsJurnal::where('tipe','BBMT')->whereYear('created_at',date('Y'))->max('no') + 1;
+        $no_1 = ModelsJurnal::where('tipe', 'JNL')->whereMonth('created_at', date('m'))->whereYear('created_at', date('Y'))->max('no') + 1;
+        $no_2 = ModelsJurnal::where('tipe', 'BBK')->whereYear('created_at', date('Y'))->max('no') + 1;
+        $no_3 = ModelsJurnal::where('tipe', 'BBM')->whereYear('created_at', date('Y'))->max('no') + 1;
+        $no_4 = ModelsJurnal::where('tipe', 'BKK')->whereYear('created_at', date('Y'))->max('no') + 1;
+        $no_5 = ModelsJurnal::where('tipe', 'BKM')->whereYear('created_at', date('Y'))->max('no') + 1;
+        $no_6 = ModelsJurnal::where('tipe', 'BBKT')->whereYear('created_at', date('Y'))->max('no') + 1;
+        $no_7 = ModelsJurnal::where('tipe', 'BBMT')->whereYear('created_at', date('Y'))->max('no') + 1;
         $now = Carbon::now()->addMonths(1)->format('Y-m-d');
         $last = Carbon::now()->subMonths(14)->format('Y-m-d');
         $last_relasi = Carbon::now()->subMonths(3)->format('Y-m-d');
@@ -37,8 +37,8 @@ class Jurnal extends Component
         $this->template = null;
         $this->is_apply = false;
         $this->templates = TemplateJurnal::all();
-        $this->coa = COA::where('is_active',1)->orderBy('kode')->get();
-        $this->orders = Order::whereBetween('created_at',[$last,$now])->select('id','no_job','job','seal','invoice')->orderBy('job')->orderBy('no_job')->get();
+        $this->coa = COA::where('is_active', 1)->orderBy('kode')->get();
+        $this->orders = Order::whereBetween('created_at', [$last, $now])->select('id', 'no_job', 'job', 'seal', 'invoice', 'container')->orderBy('job')->orderBy('no_job')->get();
         $this->debit_idx = 2;
         $this->credit_idx = 2;
         $this->form = array();
@@ -47,19 +47,19 @@ class Jurnal extends Component
         $this->coa_id = null;
         $this->tipe = null;
         $this->template_count = 0;
-        $this->no_1 = sprintf('%02d',date('m')).'-'.sprintf('%03d',$no_1).'/'.date('y');
-        $this->no_2 = sprintf('%03d',$no_2).'/BBK-'.$setting->short_name.'/'.date('y');
-        $this->no_3 = sprintf('%03d',$no_3).'/BBM-'.$setting->short_name.'/'.date('y');
-        $this->no_4 = sprintf('%03d',$no_4).'/BKK-'.$setting->short_name.'/'.date('y');
-        $this->no_5 = sprintf('%03d',$no_5).'/BKM-'.$setting->short_name.'/'.date('y');
-        $this->no_6 = sprintf('%03d',$no_6).'/BBKT-'.$setting->short_name.'/'.date('y');
-        $this->no_7 = sprintf('%03d',$no_7).'/BBMT-'.$setting->short_name.'/'.date('y');
+        $this->no_1 = sprintf('%02d', date('m')) . '-' . sprintf('%03d', $no_1) . '/' . date('y');
+        $this->no_2 = sprintf('%03d', $no_2) . '/BBK-' . $setting->short_name . '/' . date('y');
+        $this->no_3 = sprintf('%03d', $no_3) . '/BBM-' . $setting->short_name . '/' . date('y');
+        $this->no_4 = sprintf('%03d', $no_4) . '/BKK-' . $setting->short_name . '/' . date('y');
+        $this->no_5 = sprintf('%03d', $no_5) . '/BKM-' . $setting->short_name . '/' . date('y');
+        $this->no_6 = sprintf('%03d', $no_6) . '/BBKT-' . $setting->short_name . '/' . date('y');
+        $this->no_7 = sprintf('%03d', $no_7) . '/BBMT-' . $setting->short_name . '/' . date('y');
         $bgs = ModelsJurnal::whereNotNull('no_bg')->orderBy('no_bg')->pluck('no_bg')->toArray();
         $this->bgs = array_unique($bgs);
         $this->c16 = COA::where('coa_ras', 16)->first()->id ?? 16;
         $this->c45 = COA::where('coa_ras', 45)->first()->id ?? 45;
         $this->c175 = COA::where('coa_ras', 175)->first()->id ?? 175;
-        $this->relasi = ModelsJurnal::where('created_at','>=', $last_relasi)->distinct('nomor')->orderBy('nomor')->pluck('nomor')->toArray();
+        $this->relasi = ModelsJurnal::where('created_at', '>=', $last_relasi)->distinct('nomor')->orderBy('nomor')->pluck('nomor')->toArray();
     }
 
     public function render()
@@ -82,13 +82,14 @@ class Jurnal extends Component
         if (!is_null($this->template_id)) {
             $this->template_count = 1;
             $this->template = TemplateJurnal::find($this->template_id);
-        }else{
+        } else {
             $this->template = null;
         }
     }
 
-    public function addBarisTemplate(){
-        if($this->template){
+    public function addBarisTemplate()
+    {
+        if ($this->template) {
             $this->template_count += 1;
         }
         // dd($this->template_count);
