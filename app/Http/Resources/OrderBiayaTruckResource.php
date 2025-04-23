6e4c07d141f30d35a81eaace6bac3782bb300c23
AldiHasan123Dev <aldiage007.if20@student.unusa.ac.id>
2025-04-23 16:28:50 +0700

    update: monitor biaya truck

--- app/Http/Resources/OrderBiayaTruckResource.php
+++ app/Http/Resources/OrderBiayaTruckResource.php
@@ -14,26 +14,38 @@ class OrderBiayaTruckResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+        $orderTruck = $this->orderTruck;
+
         return [
             'id' => $this->id,
-            'job' => $this->orderTruck->order ? $this->orderTruck->order->job.'-'.sprintf('%02d',$this->orderTruck->order->no_job) : '-',
+            'job' => $orderTruck && $orderTruck->order
+                ? $orderTruck->order->job . '-' . sprintf('%02d', $orderTruck->order->no_job)
+                : '-',
+
             'order_trucking_id' => $this->order_trucking_id,
-            'tujuan' => $this->orderTruck->tarif->tujuan->tujuanInfo->nama ?? '-',
-            'container' => $this->orderTruck->container ?? '-',
-            'seal' => $this->orderTruck->seal ?? '-',
-            'customer' => $this->orderTruck->customer->nama,
-            'sopir' => $this->orderTruck->sopir->nama ?? '-',
-            'nopol' => $this->orderTruck->kendaraan->nopol.' | '.$this->orderTruck->kendaraan->milik,
+            'tujuan' => optional($orderTruck?->tarif?->tujuan?->tujuanInfo)->nama ?? '-',
+            'container' => $orderTruck?->container ?? '-',
+            'seal' => $orderTruck?->seal ?? '-',
+            'customer' => $orderTruck?->customer?->nama ?? '-',
+            'sopir' => $orderTruck?->sopir?->nama ?? '-',
+            'nopol' => $orderTruck && $orderTruck->kendaraan
+                ? $orderTruck->kendaraan->nopol . ' | ' . $orderTruck->kendaraan->milik
+                : '-',
+
             'tgl_sangu_kuli1' => $this->tgl_sangu_kuli1,
             'tgl_sangu_kuli2' => $this->tgl_sangu_kuli2,
             'tgl_sangu_kuli3' => $this->tgl_sangu_kuli3,
+
             'nominal_sangu_kuli1' => $this->nominal_sangu_kuli1,
             'nominal_sangu_kuli2' => $this->nominal_sangu_kuli2,
             'nominal_sangu_kuli3' => $this->nominal_sangu_kuli3,
+
             'nominal_tb_tl1' => $this->nominal_tb_tl1,
             'tgl_tb_tl' => $this->tgl_tb_tl,
+
             'nominal_stappel1' => $this->nominal_stappel1,
             'tgl_stappel' => $this->tgl_stappel,
+
             'created_by' => $this->created_by,
             'updated_by' => $this->updated_by,
             'created_at' => $this->created_at,
