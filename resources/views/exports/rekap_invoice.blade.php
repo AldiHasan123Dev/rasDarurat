e17807ccb5cdb4faf5a7d654c476e41410f6b75a
AldiHasan123Dev <aldiage007.if20@student.unusa.ac.id>
2025-04-29 13:15:23 +0700

    update: rekapan invoice(tarif dan penerima)

--- resources/views/exports/rekap_invoice.blade.php
+++ resources/views/exports/rekap_invoice.blade.php
@@ -17,11 +17,13 @@
         <tr>
             <th rowspan="2">No</th>
             <th colspan="2">Surat Jalan</th>
+            <th rowspan="2">Penerima</th>
             <th rowspan="2">No Container</th>
             <th rowspan="2">Tanggal TD</th>
             <th rowspan="2">Nama Kapal/ Voyage</th>
             <th rowspan="2">Shipment</th>
             <th rowspan="2">No Polisi</th>
+            <th rowspan="2">Tarif</th>
             <th rowspan="2">Tujuan</th>
             <th rowspan="2">Muatan / Qty</th>
             <th rowspan="2">Charge</th>
@@ -43,11 +45,13 @@
                 <td rowspan="{{ $order->bttb->count() }}">{{ $no }}</td>
                 <td rowspan="{{ $order->bttb->count() }}">{{ $order->stuffing ? date('d/m/Y', strtotime($order->stuffing)) : '' }}</td>
                 <td rowspan="{{ $order->bttb->count() }}"></td>
+                <td rowspan="{{ $order->bttb->count() }}">{{ $order->penerima->nama }}</td>
                 <td rowspan="{{ $order->bttb->count() }}">{{ $order->container }}</td>
                 <td rowspan="{{ $order->bttb->count() }}">{{ $order->jadwal_kapal ? ($order->jadwal_kapal->td ? date('d/m/y', strtotime($order->jadwal_kapal->td)) : '') : '' }}</td>
                 <td rowspan="{{ $order->bttb->count() }}">{{ $order->jadwal_kapal->kapal->nama ?? '' }} / {{ $order->jadwal_kapal->voyage ?? '' }}</td>
                 <td rowspan="{{ $order->bttb->count() }}">{{ $order->tarif->shipmentInfo->nama ?? '-' }}</td>
                 <td rowspan="{{ $order->bttb->count() }}">{{ $order->nopol }}</td>
+                <td rowspan="{{ $order->bttb->count() }}">{{ $order->tarif->tarif ?? '' }}</td>
                 <td rowspan="{{ $order->bttb->count() }}">{{ $order->tarif->tujuan_lokasi->nama ?? '' }}</td>
                 @endif
                 <td>{{ $item->qty }}</td>
