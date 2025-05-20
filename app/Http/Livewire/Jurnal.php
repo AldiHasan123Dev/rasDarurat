bee49dcde976923cedb1f2152aa5b20297af5ca1
AldiHasan123Dev <aldiage007.if20@student.unusa.ac.id>
2025-05-20 08:51:49 +0700

    update:jurnal expdc

--- app/Http/Livewire/Jurnal.php
+++ app/Http/Livewire/Jurnal.php
@@ -29,8 +29,8 @@ class Jurnal extends Component
         $no_6 = ModelsJurnal::where('tipe', 'BBKT')->whereYear('created_at', date('Y'))->max('no') + 1;
         $no_7 = ModelsJurnal::where('tipe', 'BBMT')->whereYear('created_at', date('Y'))->max('no') + 1;
         $now = Carbon::now()->addMonths(1)->format('Y-m-d');
-        $last = Carbon::now()->subMonths(6)->format('Y-m-d');
-        $last_relasi = Carbon::now()->subMonths(6)->format('Y-m-d');
+        $last = Carbon::now()->subMonths(5)->format('Y-m-d');
+        $last_relasi = Carbon::now()->subMonths(5)->format('Y-m-d');
         $setting = Setting::find(1);
         $this->invx = ModelsJurnal::whereNotNull('invoice_external')
         ->orderBy('invoice_external')
