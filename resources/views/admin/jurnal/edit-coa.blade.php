1afe6a76d808fa4c13afc9bff04a14f2782c53b1
AldiHasan123Dev <aldiage007.if20@student.unusa.ac.id>
2025-06-16 08:46:13 +0700

    update: jurnal

--- resources/views/admin/jurnal/edit-coa.blade.php
+++ resources/views/admin/jurnal/edit-coa.blade.php
@@ -104,7 +104,7 @@
 
         <div class="mt-3">
             <button class="btn btn-primary">Simpan Perubahan</button>
-            <a href="{{ route('jurnal.index') }}" class="btn btn-secondary">Kembali</a>
+            {{-- <a href="{{ route('jurnal.index') }}" class="btn btn-danger">Close</a> --}}
         </div>
     </form>
 </div>
