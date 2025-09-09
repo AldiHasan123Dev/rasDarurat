e6f3619f4aa0d5af4d1cbf57065b2cf47206a953
AldiHasan123Dev <aldiage007.if20@student.unusa.ac.id>
2025-09-09 13:39:48 +0700

    update: export order asuransi

--- resources/views/exports/asuransi.blade.php
+++ resources/views/exports/asuransi.blade.php
@@ -5,6 +5,7 @@
         <th>NO JOB</th>
         <th>NAMA ASURANSI</th>
         <th>NAMA CUSTOMER</th>
+        <th>PENGIRIM</th>
         <th>PENERIMA</th>
         <th>PELAYARAN</th>
         <th>NAMA KAPAL</th>
@@ -33,6 +34,7 @@
             <td>{{ $order->job }}-{{ sprintf('%02d',$order->no_job) }}</td>
             <td>{{ $order->asuransiInfo->nama }}</td>
             <td>{{ $order->tarif->customer->nama }}</td>
+            <td>{{ $order->pengirim->nama }}</td>
             <td>{{ $order->penerima->nama }}</td>
             <td>{{ $order->jadwal_kapal->pelayaran->nama }}</td>
             <td>{{ $order->jadwal_kapal->kapal->nama }}</td>
