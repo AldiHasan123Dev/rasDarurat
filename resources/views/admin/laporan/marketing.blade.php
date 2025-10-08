d4102fee46e4ee0c2efe6d6a6e8abae51cd814b7
AldiHasan123Dev <aldiage007.if20@student.unusa.ac.id>
2025-10-08 10:33:23 +0700

    update: lap marketing

--- resources/views/admin/laporan/marketing.blade.php
+++ resources/views/admin/laporan/marketing.blade.php
@@ -113,7 +113,7 @@
                                         @for ($i = 1; $i <= 24; $i++)
                                             @if ($i % 2 == 0)
                                                 @php
-                                                   $val = App\Models\User::find($item['id'])->laporanMarketing40Fit($month, $year);
+                                                   $val = App\Models\User::find($item['id'])->laporanMarketing20Fit($month, $year);
 
                                                     $fit20 += $val;
                                                     $sub[$i] = ($sub[$i] ?? 0) + $val;
@@ -122,8 +122,8 @@
                                                 <td class="text-center">{{ $val }}</td>
                                             @else
                                                 @php
-                                                    $val = App\Models\User::find($item['id'])->laporanMarketing20Fit($month, $year);
-
+                                                    $val = App\Models\User::find($item['id'])->laporanMarketing40Fit($month, $year);
+                                                    $fit40 += $val;
                                                     $sub[$i] = ($sub[$i] ?? 0) + $val;
                                                 @endphp
                                                 <td class="text-center">{{ $val }}</td>
