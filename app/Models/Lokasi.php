0b27cb5c7d3a72dee542dc6410460449537030de
AldiHasan123Dev <aldiage007.if20@student.unusa.ac.id>
2025-11-10 16:25:37 +0700

    update laporan tujuan dan port

--- app/Models/Lokasi.php
+++ app/Models/Lokasi.php
@@ -1,4 +1,38 @@
-public function laporan20Fit($bulan, $thn = 2023, $port_id = null)
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Auth;
+
+class Lokasi extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $table = 'lokasi';
+    protected $fillable = [
+        'nama',
+        'publis_rate',
+        'diskon',
+        'harga',
+    ];
+
+    protected static function booted()
+    {
+        static::creating(function ($model) {
+            $model->created_by = Auth::id();
+            $model->updated_by = Auth::id();
+        });
+        static::saving(function ($model) {
+            $model->updated_by = Auth::id();
+        });
+    }
+
+    
+
+    public function laporan20Fit($bulan, $thn = 2023, $port_id = null)
 {
     $job = $thn . sprintf('%02d', $bulan) . '%';
 
@@ -32,4 +66,6 @@ public function laporan40Fit($bulan, $thn = 2023, $port_id = null)
         ->count();
 
     return $order;
+}
+
 }
