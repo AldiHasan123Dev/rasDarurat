f124e6cb5a8141a5682ebce28c89386268850798
AldiHasan123Dev <aldiage007.if20@student.unusa.ac.id>
2025-12-04 10:20:52 +0700

    update: export ppn excel

--- app/Exports/LaporanPPNExport.php
+++ app/Exports/LaporanPPNExport.php
@@ -16,8 +16,17 @@ class LaporanPPNExport implements FromView
         $this->end = $end;
     }
     public function view(): View
-    {
-        $transaksi = Transaksi::whereBetween('created_at',[$this->start,$this->end])->orderBy('created_at')->get();
+{
+    try {
+        $transaksi = Transaksi::whereBetween('created_at', [$this->start, $this->end])
+            ->orderBy('created_at')
+            ->get();
+
         return view('exports.laporan_ppn', compact('transaksi'));
+
+    } catch (\Throwable $e) {
+        dd("Error view export:", $e->getMessage(), $e->getLine(), $e->getFile());
     }
+}
+
 }
