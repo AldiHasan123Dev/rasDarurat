4f8abcbf4342cacbd3ddb3705a0cb313f4b1d0cf
AldiHasan123Dev <aldiage007.if20@student.unusa.ac.id>
2026-02-07 11:09:38 +0700

    update: omset

--- resources/views/admin/laporan/omset.blade.php
+++ resources/views/admin/laporan/omset.blade.php
@@ -718,7 +718,7 @@
                                                 @endphp
                                                 {{ number_format($tarif ?? 0, 2, ',', '.') }}
                                             </td>
-                                            <td>{{ number_format($order->omset->laba_kotor ?? 0, 2, ',', '.') }}</td>
+                                            <td>{{ number_format($order->omset->laba_kotor ?? $tarif, 2, ',', '.') }}</td>
                                             <td
                                                 style="{{ $order->pra_omset
                                                     ? ($order->omset && $order->omset->margin !== null
