f0eb217c2b19a42f601e5e04b82d72e72fe28e89
AldiHasan123Dev <aldiage007.if20@student.unusa.ac.id>
2026-02-19 11:43:43 +0700

    update: pre invoice

--- app/Http/Controllers/Api/OmsetController.php
+++ app/Http/Controllers/Api/OmsetController.php
@@ -491,7 +491,10 @@ $data[$idx]['j_ut'] = Jurnal::where('order_id', $order->id)
             $tarif = (float) ($data[$idx]['tarif'] ?? 0);
             $laba  = (float) ($data[$idx]['laba_kotor'] ?? 0);
             $data[$idx]['margin'] = $tarif > 0 ? $laba / $tarif : 0;
-            $data[$idx]['margin'] = $data[$idx]['laba_kotor'] / $data[$idx]['biaya'];
+            $data[$idx]['margin'] =
+            $data[$idx]['biaya'] != 0
+            ? $data[$idx]['laba_kotor'] / $data[$idx]['biaya']
+            : 0;
              if(request('is_pra')){
                 $biaya_lain = json_encode(
                     array_merge(
