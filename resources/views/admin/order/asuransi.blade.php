c0abc1bffe313947b9900d74a296311dcc6a536f
AldiHasan123Dev <aldiage007.if20@student.unusa.ac.id>
2026-06-13 10:22:11 +0700

    update: asuransi fix

--- resources/views/admin/order/asuransi.blade.php
+++ resources/views/admin/order/asuransi.blade.php
@@ -54,12 +54,12 @@
                                 <tbody>
                                     @foreach ($orders as $order)
                                         <tr>
-                                            <td>{{ $order->id ?? '-' }}</td>
-                                            <td>{{ $order->jadwal_kapal->pelayaran_id ?? '-' }}</td>
+                                            <td>{{ $order->id }}</td>
+                                            <td>{{ $order->jadwal_kapal->pelayaran_id }}</td>
                                             <td>{{ $loop->iteration }}</td>
-                                            <td>{{ $order->job ?? '-' }}</td>
+                                            <td>{{ $order->job }}</td>
                                             <td>{{ $order->job }}-{{ sprintf('%02d',$order->no_job) }}</td>
-                                            <td>{{ $order->asuransi ?? '-' }}</td>
+                                            <td>{{ $order->asuransi }}</td>
                                             <td>{{ $order->tarif->customer->nama ?? '-' }}</td>
                                             <td>{{ $order->pengirim->nama ?? '-' }}</td>
                                             <td>{{ $order->penerima->nama ?? '-' }}</td>
@@ -68,12 +68,10 @@
                                             <td>{{ $order->tarif->shipmentInfo->nama ?? '-' }}</td>
                                             <td>{{ $order->tarif->kondisiInfo->nama ?? '-' }}</td>
                                             <td>{{ $order->barang->nama ?? '-' }}</td>
-                                            <td>{{ $order->jadwal_kapal->pelayaran->nama ?? '-' }}</td>
-                                            <td>
-    {{ $order->jadwal_kapal?->td ? date('d-m-y', strtotime($order->jadwal_kapal->td)) : '-' }}
-</td>
+                                            <td>{{ $order->jadwal_kapal->pelayaran->nama }}</td>
+                                            <td>{{ is_null($order->jadwal_kapal->td)?'-':date('d-m-y',strtotime($order->jadwal_kapal->td)) }}</td>
                                             <td>{{ $order->jadwal_kapal->kapal->nama ?? '-' }}</td>
-                                            <td>{{ $order->jadwal_kapal->voyage ?? '-' }}</td>
+                                            <td>{{ $order->jadwal_kapal->voyage }}</td>
                                             <td>{{ $order->container }}</td>
                                             <td>{{ $order->seal }}</td>
                                             <td>{{ $order->keterangan }}</td>
